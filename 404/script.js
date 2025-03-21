b1c1933a8c330f7133b9635e204b23e141ac7cbe
Nana permana <72350180+nanaperm12@users.noreply.github.com>
2025-03-22 05:52:53 +0700

    Update script.js

--- 404/script.js
+++ 404/script.js
@@ -1,99 +1,36 @@
-$(function () {
-    var liWidth = $("li").css("width");
-    $("li").css("height", liWidth);
-    $("li").css("lineHeight", liWidth);
-    var totalHeight = $("#wordsearch").css("width");
-    $("#wordsearch").css("height", totalHeight);
-});
-causeRepaintsOn = $("h1, h2, h3, p");
-$(window).resize(function () {
-    causeRepaintsOn.css("z-index", 1);
-});
-$(window).on('resize', function () {
-    var liWidth = $(".one").css("width");
-    $("li").css("height", liWidth);
-    $("li").css("lineHeight", liWidth);
-    var totalHeight = $("#wordsearch").css("width");
-    $("#wordsearch").css("height", totalHeight);
-});
+var $copyContainer = $(".copy-container"),
+    $replayIcon = $('#cb-replay'),
+    $copyWidth = $('.copy-container').find('p').width();
 
+var mySplitText = new SplitText($copyContainer, {type:"words"}),
+    splitTextTimeline = new TimelineMax();
+var handleTL = new TimelineMax();
 
+// WIP - need to clean up, work on initial state and handle issue with multiple lines on mobile
 
-$(function () {
-    /* 4 */
-    $(this).delay(1500).queue(function () {
-        $(".one").addClass("selected");
-        $(this).dequeue();
-    })
-    /* 0 */
-    .delay(500).queue(function () {
-        $(".two").addClass("selected");
-        $(this).dequeue();
-    })
-    /* 4 */
-    .delay(500).queue(function () {
-        $(".three").addClass("selected");
-        $(this).dequeue();
-    })
-    /* P */
-    .delay(500).queue(function () {
-        $(".four").addClass("selected");
-        $(this).dequeue();
-    })
-    /* A */
-    .delay(500).queue(function () {
-        $(".five").addClass("selected");
-        $(this).dequeue();
-    })
-    /* G */
-    .delay(500).queue(function () {
-        $(".six").addClass("selected");
-        $(this).dequeue();
-    })
-    /* E */
-    .delay(500).queue(function () {
-        $(".seven").addClass("selected");
-        $(this).dequeue();
-    })
-    /* N */
-    .delay(500).queue(function () {
-        $(".eight").addClass("selected");
-        $(this).dequeue();
-    })
-    /* O */
-    .delay(500).queue(function () {
-        $(".nine").addClass("selected");
-        $(this).dequeue();
-    })
-    /* T */
-    .delay(500).queue(function () {
-        $(".ten").addClass("selected");
-        $(this).dequeue();
-    })
-    /* F */
-    .delay(500).queue(function () {
-        $(".eleven").addClass("selected");
-        $(this).dequeue();
-    })
-    /* O */
-    .delay(500).queue(function () {
-        $(".twelve").addClass("selected");
-        $(this).dequeue();
-    })
-    /* U */
-    .delay(500).queue(function () {
-        $(".thirteen").addClass("selected");
-        $(this).dequeue();
-    })
-    /* N */
-    .delay(500).queue(function () {
-        $(".fourteen").addClass("selected");
-        $(this).dequeue();
-    })
-    /* D */
-    .delay(500).queue(function () {
-        $(".fifteen").addClass("selected");
-        $(this).dequeue()
-    });
-});
+var tl = new TimelineMax();
 
+tl.add(function(){
+  animateCopy();
+  blinkHandle();
+}, 0.2)
+
+function animateCopy() {
+  mySplitText.split({type:"chars, words"}) 
+  splitTextTimeline.staggerFrom(mySplitText.chars, 0.001, {autoAlpha:0, ease:Back.easeInOut.config(1.7), onComplete: function(){
+    animateHandle()
+  }}, 0.05);
+}
+
+function blinkHandle() {
+  handleTL.fromTo('.handle', 0.4, {autoAlpha:0},{autoAlpha:1, repeat:-1, yoyo:true}, 0);
+}
+
+function animateHandle() {
+  handleTL.to('.handle', 0.7, {x:$copyWidth, ease:SteppedEase.config(12)}, 0.05);
+}
+
+$('#cb-replay').on('click', function(){
+  splitTextTimeline.restart()
+  handleTL.restart()
+})
